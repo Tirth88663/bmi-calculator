b711296ff23d0eb96204c5d8cb66b4ccac46e4ed
Tirth88663 <tirthp88663@gmail.com>
2022-02-01 23:49:38 +0530

    added sliderBar

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -17,6 +17,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender SelectedGender;
+  int height = 180;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -78,7 +79,34 @@ class _InputPageState extends State<InputPage> {
                     Text(
                       "HEIGHT",
                       style: kCardTextStyle,
-                    )
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: <Widget>[
+                        Text(
+                          height.toString(),
+                          style: kNumberTextStyle,
+                        ),
+                        Text(
+                          "cm",
+                          style: kCardTextStyle,
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      value: height.toDouble(),
+                      min: 120,
+                      max: 210,
+                      activeColor: Color(0xFFEB1555),
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
                   ],
                 ),
               ),
@@ -90,11 +118,37 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: MyCard(
                       colour: kInactiveCardColor,
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "WEIGHT",
+                            style: kCardTextStyle,
+                          ),
+                          Text(
+                            "87",
+                            style: kNumberTextStyle,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   Expanded(
                     child: MyCard(
                       colour: kInactiveCardColor,
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "AGE",
+                            style: kCardTextStyle,
+                          ),
+                          Text(
+                            "18",
+                            style: kNumberTextStyle,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
